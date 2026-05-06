9f7ebeea078e8fe13be81bca7fa07b037422a8e3
Supreet Kumar <supreetkumar548@gmail.com>
2026-05-06 17:37:54 +0530

    Add helpful assistant SystemMessage

---
+++ DailyTask/task1.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import SystemMessage, HumanMessage
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(
+    model=os.getenv("MODEL_CHAT"),
+    google_api_key=os.getenv("GOOGLE_API_KEY"),
+    temperature=0.7
+)
+
+response = llm.invoke([
+    SystemMessage(content="You are a helpful assistant"),
+    HumanMessage(content="Explain recursion in 2 lines")
+])
+
+print(response.content)
+
+
+
